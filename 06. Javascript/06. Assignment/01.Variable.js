ef705bb71d799483063a8ed9a6f6efbb5e42087f
BinodKumarD <vk055249@gmail.com>
2023-01-17 22:24:29 +0530

    Variable in assignment of javascript added

---
+++ 06. Javascript/06. Assignment/01.Variable.js
@@ -0,0 +1,17 @@
+// declare variables and assign string, boolean, undefined and null data types, Display all the value with their data type.
+
+// first name is assigned with string value
+var firstName = "Binod"; 
+console.log(`First name: ${firstName} (Type of firstName: ${typeof firstName})`);
+
+// isLoggedOn is assigned with boolean value
+const isLoggedOn = true;
+console.log(`Is Loggedin: ${isLoggedOn} (Type of isLoggedOn: ${typeof isLoggedOn})`);
+
+//In lastName we will not be defining any value so by default it will be undefined
+let lastName;
+console.log(`Last Name: ${lastName} (Type of last name: ${typeof lastName})`);
+
+//age value is assigned with null value
+const age = null;
+console.log(`Age: ${age} (Type of Age: ${typeof age})`);
