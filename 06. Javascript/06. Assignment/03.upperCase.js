c9d0299c2048733982b95377210bf6b648277012
BinodKumarD <vk055249@gmail.com>
2023-01-19 20:59:06 +0530

    usage of upperCase function in assignment of javascript added

---
+++ 06. Javascript/06. Assignment/03.upperCase.js
@@ -0,0 +1,14 @@
+// Declare a varibale and assign string value to it and change all the string characters to capital letters using toUpperCase() method.
+
+const testing = "hello";
+let testing2 = "world";
+var testing3 = "testing and changing";
+
+console.log(`\n"${testing} ${testing2} ${testing3}" in small letter`);
+
+
+console.log(`\n-------------------------------------------------------------------`);
+//using touppercase method to change from lower case to uppercase
+console.log(`"${testing.toUpperCase()} ${testing2.toUpperCase()} ${testing3.toUpperCase()}" to upper case letter`);
+console.log(`-------------------------------------------------------------------`);
+
