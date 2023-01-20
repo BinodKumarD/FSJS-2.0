0b11fcdafa08cc8900e366a59e299d88837998a9
BinodKumarD <vk055249@gmail.com>
2023-01-20 22:17:02 +0530

    usage of includes function in assignment of javascript added

---
+++ 06. Javascript/06. Assignment/04.includes.js
@@ -0,0 +1,9 @@
+// Declare a varibale and assign string value to it and check if the string contains a word Script using includes() method
+
+const fullName = "Binod kumar";
+let result = fullName.includes("kumar");
+console.log(`${result}`);
+
+const textCheck = "Declaring variable and assigning string value to it and using includes function to check the particular part in sentence";
+var result1 = textCheck.includes("variable");
+console.log(`${result1}`);
