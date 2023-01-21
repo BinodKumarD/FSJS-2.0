0df6fb732eb229d48254f23dc33dde610e69bcf8
BinodKumarD <vk055249@gmail.com>
2023-01-21 22:16:13 +0530

    usage of split function in assignment of javascript added

---
+++ 06. Javascript/06. Assignment/05.split.js
@@ -0,0 +1,6 @@
+// Declare a varibale and assign string value to it and then split it into an array using split() method
+ 
+
+let splitItIntoAnArray = "Binod kumar";
+console.log(splitItIntoAnArray.split()); // ['Binod Kumar']
+console.log(splitItIntoAnArray.split("")); //['B', 'i', 'n', 'o', 'd', ' ', 'k', 'u', 'm','a', 'r']
